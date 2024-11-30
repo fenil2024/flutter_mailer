9b8bc28e5e1721f1b1166230ed23795606d1f5e5
fenil-gol-oceanmtech <fenilgol5@gmail.com>
2024-11-30 10:14:29 +0530

    version Changes
    version Changes

--- example/android/app/src/main/java/com/dataxad/flutter_mailer_example/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/dataxad/flutter_mailer_example/EmbeddingV1Activity.java
@@ -1,19 +1,5 @@
-package com.dataxad.flutter_mailer_example;
-
-import android.os.Bundle;
-import com.dataxad.flutter_mailer.FlutterMailerPlugin;
-import dev.flutter.plugins.integration_test.IntegrationTestPlugin;
-import io.flutter.plugins.imagepicker.ImagePickerPlugin;
-import io.flutter.plugins.pathprovider.PathProviderPlugin;
+package com.dataxad.flutter_mailer_example; 
 import io.flutter.app.FlutterActivity;
 
 public class EmbeddingV1Activity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    FlutterMailerPlugin.registerWith(registrarFor("com.dataxad.flutter_mailer.FlutterMailerPlugin"));
-    IntegrationTestPlugin.registerWith(registrarFor("dev.flutter.plugins.integration_test.IntegrationTestPlugin"));
-    ImagePickerPlugin.registerWith(registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"));
-    PathProviderPlugin.registerWith(registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
-  }
 }
--- example/android/app/src/main/java/com/dataxad/flutter_mailer_example/MainActivity.java
+++ example/android/app/src/main/java/com/dataxad/flutter_mailer_example/MainActivity.java
@@ -2,5 +2,5 @@ package com.dataxad.flutter_mailer_example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity { 
 }
