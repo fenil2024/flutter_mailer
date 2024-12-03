91c572bea1e34dd2606e4ffeca20abd3eac81a81
fenil-gol-oceanmtech <fenilgol5@gmail.com>
2024-12-03 17:10:38 +0530

    ADK-35 version
    ADK-35 version

--- example/android/app/src/main/java/com/dataxad/flutter_mailer_example/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/dataxad/flutter_mailer_example/EmbeddingV1Activity.java
@@ -1,5 +1,24 @@
-package com.dataxad.flutter_mailer_example; 
-import io.flutter.app.FlutterActivity;
+package com.dataxad.flutter_mailer_example;
+
+// import android.os.Bundle;
+// import com.dataxad.flutter_mailer.FlutterMailerPlugin;
+// import dev.flutter.plugins.integration_test.IntegrationTestPlugin;
+// import io.flutter.plugins.imagepicker.ImagePickerPlugin;
+// import io.flutter.plugins.pathprovider.PathProviderPlugin;
+// import io.flutter.app.FlutterActivity;
+
+// public class EmbeddingV1Activity extends FlutterActivity {
+//   @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(savedInstanceState);
+//     FlutterMailerPlugin.registerWith(registrarFor("com.dataxad.flutter_mailer.FlutterMailerPlugin"));
+//     IntegrationTestPlugin.registerWith(registrarFor("dev.flutter.plugins.integration_test.IntegrationTestPlugin"));
+//     ImagePickerPlugin.registerWith(registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"));
+//     PathProviderPlugin.registerWith(registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
+//   }
+// }
+
+import io.flutter.embedding.android.FlutterActivity;
 
 public class EmbeddingV1Activity extends FlutterActivity {
 }
